c3f56438a9b7dff0144d734d2c1b6ae90c412bd1
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-12-23 15:38:25 -0500

    Update search.js for OCDS Index. Re-indent. Use different variable names.

--- standard_theme/static/js/search.js
+++ standard_theme/static/js/search.js
@@ -1,56 +1,71 @@
-$( document ).ready(function() {
-    var queryDict = {};
-    location.search.substr(1).split("&").forEach(function(item) {queryDict[item.split("=")[0]] = item.split("=")[1]})
-    var term = $('#rtd-search-form input[name="q"]').val(decodeURIComponent(queryDict.q.replace(/\+/g, '%20')));
-    renderResults();
-    // This will make the search change without new url request but url parameters stay the same.
-    //$('#rtd-search-form').submit(function( event ) {
-    //    renderResults();
-    //    event.preventDefault();
-    //});
+$(document).ready(function () {
+  renderResults();
 });
 
-function escapeString(text) {
-    return $("<div>").text(text).html();
-}
-
 function renderResults() {
-    var term = $('#rtd-search-form input[name="q"]').val();
-    var base_url = location.href.substring(0, location.href.indexOf('/search/?') + 1)
-    $.ajax({
-        url: "https://standard-search.open-contracting.org/v1/search?q=" + encodeURIComponent(term) + "&base_url=" + encodeURIComponent(base_url),
-        //url: "http://localhost:5000/v1/search?q=" + encodeURIComponent(term) + "&base_url=" + encodeURIComponent(base_url),
-        success: function(data) {
-            $('#search-results').hide();
-            $('#search-results').html('<div id="resultsCount"></div> <ul id="resultsList" class="search"></ul>');
+  var query = $('#rtd-search-form input[name="q"]').val();
+  var base_url = location.href.substring(0, location.href.indexOf('/search/?') - 2)
 
-            var resultString = "Search finished, found %s page(s) matching the search query.";
-
-            var translatedResultString = Documentation.TRANSLATIONS[resultString] || resultString;
-
-            $('#resultsCount').html(translatedResultString.replace('%s', data.count.toString()));
-            var html = '';
-            
-
-            for(idx in data.results) {
-                var split = data.results[idx].url.split('#')
-                var new_url = split[0] + '?highlight=' + encodeURIComponent(term) + '#' + split[1]
-
-                html += '<li>';
-                html += '<a href="'+new_url+'">';
-                html += escapeString(data.results[idx].title);
-                html += '</a>';
-                html += '<div class="context">';
-
-                for(hidx in data.results[idx].highlights) {
-                    html += data.results[idx].highlights[hidx] + ' ';
-                }
-                html += '</div>';
-                html += '</li>';
+  $.ajax({
+    url: 'https://standard.open-contracting.org:9200/ocdsindex_en/_search?size=100',
+    username: 'public',
+    // The "public" user has read-only access to Elasticsearch indices created by OCDS Index. We set a password
+    // only to limit the impact of untargeted scans (e.g. bots).
+    password: 'G*PweUnH4u@r',
+    contentType: 'application/json',
+    data: JSON.stringify({
+      "query": {
+        "bool": {
+          "must": {
+            "simple_query_string": {
+              "query": query,
+              "fields": ["text", "title^3"],
+              "default_operator": "and"
             }
-            $('#resultsList').html(html);
-            $('#search-results').show();
+          },
+          "filter": {
+            "term": {
+              "base_url": base_url
+            }
+          }
         }
-    });
-}
+      },
+      "highlight": {
+        "fields": {
+          "text": {},
+          "title": {}
+        }
+      }
+    }),
+    success: function(data) {
+      $('#search-results').hide();
 
+      $('#search-results').html('<div id="resultsCount"></div><ul id="resultsList" class="search"></ul>');
+
+      var message = "Search finished, found %s page(s) matching the search query.";
+
+      var countHtml = (Documentation.TRANSLATIONS[message] || message).replace('%s', data.hits.total.value.toString());
+
+      var listHtml = '';
+      for (var i = 0, l = data.hits.hits.length; i < l; i++) {
+        var parts = data.hits.hits[i].url.split('#');
+
+        listHtml += '<li>';
+        listHtml += '<a href="' + parts[0] + '?highlight=' + encodeURIComponent(query) + '#' + parts[1] + '">';
+        listHtml += $("<div>").text(data.hits.hits[i].title).html();
+        listHtml += '</a>';
+        listHtml += '<div class="context">';
+        for (var j = 0, m = data.hits.hits[i].highlights.length; j < k; j++) {
+          listHtml += data.hits.hits[i].highlights[j] + ' ';
+        }
+        listHtml += '</div>';
+        listHtml += '</li>';
+      }
+
+      $('#resultsCount').html(countHtml);
+      $('#resultsList').html(listHtml);
+
+      $('#search-results').show();
+    }
+  });
+}
