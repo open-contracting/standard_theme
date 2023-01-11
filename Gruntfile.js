6de7ab01caadd7052a09493d5d0ffdb12d277780
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-01-11 15:22:43 -0500

    build: Use bundle in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -61,6 +61,7 @@ module.exports = function(grunt) {
       dev: {
         options: {
           style: 'expanded',
+          bundleExec: true,
           loadPath: ['bower_components/bourbon/dist', 'bower_components/neat/app/assets/stylesheets', 'bower_components/font-awesome/scss', 'bower_components/wyrm/sass']
         },
         files: [{
@@ -74,6 +75,7 @@ module.exports = function(grunt) {
       build: {
         options: {
           style: 'compressed',
+          bundleExec: true,
           loadPath: ['bower_components/bourbon/dist', 'bower_components/neat/app/assets/stylesheets', 'bower_components/font-awesome/scss', 'bower_components/wyrm/sass']
         },
         files: [{
