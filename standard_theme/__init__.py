07ca0e39979a244656dd6df0658f2ead428184b9
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-09-14 19:12:12 -0400

    chore: Run ruff check .

--- standard_theme/__init__.py
+++ standard_theme/__init__.py
@@ -10,5 +10,4 @@ import os
 
 def get_html_theme_path():
     """Return list of HTML theme paths."""
-    cur_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-    return cur_dir
+    return os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
